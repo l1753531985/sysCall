aa9c4ee786058dd6d2eaf9a70de4b940b3e605a4
lxg <l1753531985@gmail.com>
2021-10-22 02:54:15 -0400

    who version 1.0

---
+++ who1.c
@@ -0,0 +1,53 @@
+/* who1.c - a first version of the who program
+ * open, read UTMP file, and show results 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SHOWHOST
+
+void show_info(struct utmp*);
+
+int main()
+{
+	struct utmp current_record;
+	int utmpfd;
+	int reclen = sizeof(current_record);
+
+	if ((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1)
+	{
+		perror(UTMP_FILE);
+		exit(1);
+	}
+
+	while (read(utmpfd, &current_record, reclen) == reclen)
+		show_info(&current_record);
+	close(utmpfd);
+	return 0;
+}
+
+/* 
+ * show_info()
+ * displays contents of the utmp struct in human readable form 
+ * *note* these sizes should not be hardwired
+ */
+void show_info(struct utmp* utbufp)
+{
+	printf("%-8.8s", utbufp->ut_user);
+	printf(" ");
+	printf("%-8.8s", utbufp->ut_line);
+	printf(" ");
+	printf("%10ld", utbufp->ut_tv);
+	printf(" ");
+#ifdef SHOWHOST
+	printf("(%s)", utbufp->ut_host);
+#endif
+	printf("\n");
+}
+
