962286dc5b281f5e30053d08a7169a5c8dfc2d25
lxg <l1753531985@gmail.com>
2021-12-06 05:13:29 -0500

    mkdir is available

---
+++ 04/mkdir1.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <dirent.h>
+#include <unistd.h>
+#include <queue>
+#include <unordered_map>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sstream>
+
+using namespace std;
+
+enum ParentOptions { PARENTS, NONE };
+unordered_map<string, ParentOptions> rawCommand{{"-p", ParentOptions::PARENTS}};
+
+ParentOptions getParentOption(queue<string>& commands)
+{
+	int size = commands.size();
+	unordered_map<string, ParentOptions>::iterator iter;
+	while (size)
+	{
+		string curItem = commands.front();
+		iter = rawCommand.find(curItem);
+		ParentOptions tmp = (iter == rawCommand.end()) ? ParentOptions::NONE : iter->second;
+		commands.pop();
+		if (tmp != ParentOptions::NONE) return tmp;
+		commands.push(curItem);
+		size--;
+	}
+}
+
+void createDirWithoutOptions(const string& dirname)
+{
+	if (mkdir(dirname.c_str(), 0644) == -1)
+	{
+		cerr << "create directory '" << dirname << "' error" << endl;
+		return;
+	}
+}
+
+void createDirWithParents(const string& path)
+{
+	//cout << "path:" << path << '\t';
+	stringstream ss{path};
+	string tmp;
+	queue<string> dirnames;
+	while (getline(ss, tmp, '/'))
+		dirnames.push(tmp);
+	while (dirnames.size() > 1)
+	{
+		string curDir = dirnames.front();
+		dirnames.pop();
+		if (curDir == "") 
+		{
+			if (chdir("/") == -1)
+			{
+				cerr << "Change to root directory error" << endl;
+				return;
+			}
+			continue;
+		}
+		struct stat buf;
+		if (stat(curDir.c_str(), &buf) == -1)
+		{
+			cerr << "The directory '" << tmp << "' is not exist" << endl;
+			return;
+		}
+		else if (S_ISDIR(buf.st_mode)) 
+		{
+			if (chdir(curDir.c_str()) == -1)
+			{
+				cerr << "Change to directory '" << tmp << "' error" << endl;
+				return;
+			}
+		}
+		else
+		{
+			createDirWithoutOptions(curDir);
+		}
+	}
+	createDirWithoutOptions(dirnames.front());
+}
+
+int main(int ac, char* argv[])
+{
+	if (ac == 1)
+	{
+		cerr << "mkdir1: missing operand" << endl;
+		return -1;
+	}
+	queue<string> commandInPiece;
+	while (ac)
+	{
+		ac--;
+		commandInPiece.push(argv[ac]);
+	}
+	ParentOptions optionInCommand = getParentOption(commandInPiece);	
+	switch (optionInCommand)
+	{
+		case ParentOptions::PARENTS:
+			createDirWithParents(commandInPiece.front());
+			break;
+		case ParentOptions::NONE:
+			createDirWithoutOptions(commandInPiece.front());
+			break;
+		default:
+			break;
+	}
+	return 0;
+}
+
