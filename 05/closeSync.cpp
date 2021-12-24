e78657dd82a9a16fcd01e5b8953a2ba23ba72c16
lxg <l1753531985@gmail.com>
2021-12-24 05:32:43 -0500

    closeSync.cpp has not be finished

--- 05/closeSync.cpp
+++ 05/closeSync.cpp
@@ -1,18 +1,60 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define SETSYNC(m) m | O_SYNC
+#define CALCELSYNC(m) m & (~O_SYNC)
 
 using namespace std;
+
+void setFileStatusFlag(bool isSet)
+{
+	// set flag
+	const string bigFile = "/root/sysCall/05/bigTest.txt";
+	int fd = -1;
+	if ((fd = open(bigFile.c_str(), O_RDONLY)) == -1)
+	{
+		perror(bigFile.c_str());
+		exit(1);
+	}
+	cout << "after open!!!" << endl;
+	int s = fcntl(fd, F_GETFL);
+	s = (isSet) ? SETSYNC(s) : CALCELSYNC(s);	
+	if (fcntl(fd, F_SETFL, s) == -1)
+	{
+		perror("setting SYNC");
+		exit(1);
+	}
+
+	// write 
+	string tmp = "";
+	const int size = 16;
+	if (write(fd, tmp.c_str(), size) > 0)
+		cout << tmp << endl;
+
+	// close file 
+	if (close(fd) == -1)
+	{
+		cerr << "close " << bigFile << "failed" << endl;
+		exit(1);
+	}	
+}
 
 int main(int ac, char* argv[])
 {
 	if (ac == 1) return 0;
 	string para = argv[1];
-	switch (para[1])
+	switch (para[0])
 	{
 		case 'y':
 		case 'Y':
+			setFileStatusFlag(true);
 			break;
 		case 'n':
 		case 'N':
+			setFileStatusFlag(false);
 			break;
 		default:
 			break;
