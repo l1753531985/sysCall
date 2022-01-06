27d0cc66083fa3e8e70adfdded936f6daf9088b5
lxg <l1753531985@gmail.com>
2022-01-06 02:01:51 -0500

    test curses first version

---
+++ 07/hello1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main()
+{
+	initscr();
+	clear();
+	for (int i = 0; i < LINES; i++)
+	{
+		move(i, i+1);
+		if (i%2 == 1)
+			standout();
+		addstr("Hello, world");
+		if (i%2 == 1)
+			standend();
+	}
+	refresh();
+	getch();
+	endwin();
+	return 0;
+}
